87ae980913bc340e68d3597d31ca833b8ef7b71c
Ian Wright <wright.ianb@gmail.com>
2019-02-28 17:20:42 -0700

    Add Contact button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import styled from 'styled-components'
 
-import { Layout, Wrapper, Job, Project, SectionTitle } from '../components'
+import { Button, Layout, Wrapper, Job, Project, SectionTitle } from '../components'
 
 const Content = styled.div`
   grid-column: 2;
@@ -59,6 +59,14 @@ const IndexPage = ({
       <Hero>
         <h1>👋 Hello</h1>
         <p>I&apos;m a software engineer specializing in front-end development, JS, and React 💻</p>
+        <Link to="/contact">
+          <Button big>
+            <svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+              <path d="M1764 11q33 24 27 64l-256 1536q-5 29-32 45-14 8-31 8-11 0-24-5l-453-185-242 295q-18 23-49 23-13 0-22-4-19-7-30.5-23.5t-11.5-36.5v-349l864-1059-1069 925-395-162q-37-14-40-55-2-40 32-59l1664-960q15-9 32-9 20 0 36 11z" />
+            </svg>
+            Contact
+          </Button>
+        </Link>
       </Hero>
       <Content>
         <SectionTitle>Recent Projects</SectionTitle>
